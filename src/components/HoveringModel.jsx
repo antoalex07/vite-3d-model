7408973a70c971538fbdb90417a3ea8e1606d9ed
antoalex07 <antoalex710@gmail.com>
2024-07-08 01:11:44 +0530

    Hovering function implemented

---
+++ src/components/HoveringModel.jsx
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from 'react'
+import Final from '../../public/Final'
+import { useFrame } from '@react-three/fiber';
+
+const HoveringModel = () => {
+    
+    const modelRef = useRef();
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+    const handleMouseMove = (event) => {
+        setMousePosition({
+          x: (event.clientX / window.innerWidth) * 2 - 1,
+          y: -(event.clientY / window.innerHeight) * 2 + 1,
+        });
+    };
+
+    useFrame(() => {
+        if (modelRef.current) {
+          modelRef.current.rotation.y = mousePosition.x * Math.PI * 3;
+          modelRef.current.rotation.x = mousePosition.y * Math.PI * 2; // Adjust the factor to control rotation speed
+        }
+    });
+    
+
+  return (
+    <mesh ref={modelRef} onPointerMove={handleMouseMove}>
+        <Final/>
+    </mesh>
+  )
+}
+
+export default HoveringModel
