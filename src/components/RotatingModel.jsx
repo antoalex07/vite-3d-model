7c4141428a86d3801d77177f3188729a412b2469
antoalex07 <antoalex710@gmail.com>
2024-07-08 00:49:31 +0530

    automatic rotation function implemented

--- src/components/RotatingModel.jsx
+++ src/components/RotatingModel.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useFrame } from '@react-three/fiber';
+import React, { useRef } from 'react'
+import Final from '../../public/Final'
 
 const RotatingModel = () => {
+    
+    const modelRef = useRef();
+
+    useFrame(() => {
+        if(modelRef.current) {
+            modelRef.current.rotation.y += 0.01
+        }
+    });
+
   return (
-    <div>RotatingModel</div>
-  )
+    <mesh ref={modelRef}>
+        <Final/>
+    </mesh>
+  );
 }
 
 export default RotatingModel
