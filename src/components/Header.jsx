17edc9558df8dd762c92c9c00f884723573d848e
antoalex07 <antoalex710@gmail.com>
2024-07-07 23:37:55 +0530

    header component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <a href='/'>
+            <div className='logo'></div>
+        </a>
+    </div>
+  )
+}
+
+export default Header
