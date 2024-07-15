0ecfa22aba424bb3a332129cd441cd5e29424062
antoalex07 <antoalex710@gmail.com>
2024-07-15 10:18:32 +0530

    path for testing configs added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Sample from './pages/Sample'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       {/* <Header/> */}
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/saple' element={<Sample/>} />
       </Routes>
     </div>  
   )
